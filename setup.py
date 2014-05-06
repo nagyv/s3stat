c6919b44f8b37b8bf5e2a3ca0534160c38b2127f
Viktor Nagy <viktor.nagy@gmail.com>
2014-05-06 21:46:58 +0200

    Added description field

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import s3stat
 setup(
     name="s3stat",
     version="1.0.0",
+    description='An extensible Amazon S3 and Cloudfront log parser.',
     long_description=s3stat.__doc__,
     author="Viktor Nagy",
     author_email='v@pulilab.com',
