fd64da9b4913e033937751c6c6fd0d6cd91ac8c2
Viktor Nagy <viktor.nagy@gmail.com>
2014-05-09 10:27:34 +0200

    Bump version: 1.1.0 → 2.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import s3stat
 
 setup(
     name="s3stat",
-    version="1.1.0",
+    version="2.0.0",
     description='An extensible Amazon S3 and Cloudfront log parser.',
     long_description=s3stat.__doc__,
     author="Viktor Nagy",
